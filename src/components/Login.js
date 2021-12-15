e3845350a873497d6fc202b07955fcf6a979be29
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-15 09:38:10 +0900

    added logo

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,8 @@ const Login = () => {
   return (
     <div>
       <h1>Login Page</h1>
+      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+      <button>Login With Spotify</button>
     </div>
   );
 };
