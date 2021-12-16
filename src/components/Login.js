c2e99d19bb2f5196d8228e384a2ac744ac551054
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-16 16:09:12 +0900

    added spotify file, minor button style

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,10 @@ import "../CSS/Login.css"
 
 const Login = () => {
   return (
-    <div>
+    <div className="login">
       <h1>Login Page</h1>
       <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
-      <button>Login With Spotify</button>
+      <a>Login With Spotify</a>
     </div>
   );
 };
---
+++ src/spotify.js
@@ -0,0 +1,5 @@
+export const authEndpoint = "https://accounts/spotify.com/authorize";
+//spotify's API to authenticate user with their real spotify profile.
+
+const redirectUri = "http:localhost/3000/";
+
