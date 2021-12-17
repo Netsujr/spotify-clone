b063795440c1278c27e102990f4629230c4d4712
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-17 12:55:37 +0900

    added sidebar function

--- src/functions/Sidebar.js
+++ src/functions/Sidebar.js
@@ -17,14 +17,14 @@ function Sidebar() {
       <img
         className="sidebar__logo"
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"alt=""/>
-      <SidebarOption Icon={HomeIcon} option="Home" />
-      <SidebarOption Icon={SearchIcon} option="Search" />
-      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
       <br />
       <strong className="sidebar_title">PLAYLISTS</strong>
       <hr />
       {playlists?.items?.map((playlist) => (
-        <SidebarOption option={playlist.name} />
+        <SidebarOption title={playlist.name} />
       ))}
     </div>
   );
