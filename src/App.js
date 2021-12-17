533a6f44eb14cc7beb317d98d1aae4b6a9977c71
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-17 12:07:12 +0900

    able to login again after refectoring

--- src/App.js
+++ src/App.js
@@ -1,54 +1,72 @@
 import React, { useEffect } from "react";
-import './App.css';
-import Login from './functions/Login';
-import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
-import Player from './functions/Player';
 import { useDataLayerValue } from "./DataLayer";
+import Player from "./functions/Player";
+import { getTokenFromUrl } from "./spotify";
+import "./CSS/App.css";
+import Login from "./functions/Login";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   //{ } is what we need fromthe dataLayer, dispatch is what we are sending over there
-  const [{ user, token }, dispatch] = useDataLayerValue();
+  const [{ token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
+    // Set token
     const hash = getTokenFromUrl();
     window.location.hash = "";
-    const _token = hash.access_token;
+    let _token = hash.access_token;
 
     if (_token) {
+      spotify.setAccessToken(_token);
 
       dispatch({
         type: "SET_TOKEN",
-        token: _token
+        token: _token,
       });
 
-      spotify.setAccessToken(_token);
+      // spotify.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+      //   dispatch({
+      //     type: "SET_DISCOVER_WEEKLY",
+      //     discover_weekly: response,
+      //   })
+      // );
+
+      // spotify.getMyTopArtists().then((response) =>
+      //   dispatch({
+      //     type: "SET_TOP_ARTISTS",
+      //     top_artists: response,
+      //   })
+      // );
+
+      // dispatch({
+      //   type: "SET_SPOTIFY",
+      //   spotify: spotify,
+      // });
 
       spotify.getMe().then((user) => {
-
         dispatch({
-          type: 'SET_USER',
-          user: user,
-
+          type: "SET_USER",
+          user,
         });
       });
+
       spotify.getUserPlaylists().then((playlists) => {
         dispatch({
           type: "SET_PLAYLISTS",
-          playlists: playlists,
-        })
-      })
-    };
-  }, []);
-
+          playlists,
+        });
+      });
+    }
+  }, [token, dispatch]);
 
   return (
-    <div className="App">
-      {token ? (<Player />) : (<Login />)}
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={spotify} />}
     </div>
   );
-};
+}
 
 export default App;
--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,16 @@
-// import { findAllByDisplayValue } from "@testing-library/react";
+import { findAllByDisplayValue } from "@testing-library/react";
 
 export const initialState = {
   user: null,
   playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
   playing: false,
   item: null,
-  // REMOVE AFTER DEVELOPMENT
-  // REMOVE AFTER DEVELOPMENT >>>>> CHange back to null
-  token: null,
-  // "BQDUfmV7QjUR0gHxEZzR_wyiixsUd6NnMtG_wEp-qG8b674fr3YyCjWR1mQcWAUTL8Mt3ERa5ieYG7-v2Tzt7hY305dHDBLAkAgxfnulTgfKswuVnsWNoGD2VhMph2WoOBGHxkxYVxu01BBR2kgUlk7kEHPoDiz3cQ",
+  // stay logged in for debugging
+    // "BQDUfmV7QjUR0gHxEZzR_wyiixsUd6NnMtG_wEp-qG8b674fr3YyCjWR1mQcWAUTL8Mt3ERa5ieYG7-v2Tzt7hY305dHDBLAkAgxfnulTgfKswuVnsWNoGD2VhMph2WoOBGHxkxYVxu01BBR2kgUlk7kEHPoDiz3cQ",
+
 };
 
 const reducer = (state, action) => {
@@ -16,29 +18,60 @@ const reducer = (state, action) => {
   // action -> type, [payload]
   // dispatch the action, actiong has a type (user, playlist etc..)
   switch (action.type) {
-    case 'SET_USER':
+    case "SET_USER":
       return {
         //keep what was in the current state
         ...state,
         user: action.user,
         //update case here
       };
-    case 'SET_TOKEN':
+
+    case "SET_PLAYING":
+      return {
+        ...state,
+        playing: action.playing,
+      };
+
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    case "SET_TOP_ARTISTS":
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case "SET_TOKEN":
       return {
         ...state,
         token: action.token,
       };
 
-    case 'SET_PLAYLISTS':
+    case "SET_SPOTIFY":
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+
+    case "SET_PLAYLISTS":
       return {
         ...state,
         playlists: action.playlists,
       };
+          // default should return something so app doesnt break
 
     default:
       return state;
-    // default should return something so app doesnt break
   }
-}
+};
 
 export default reducer;
