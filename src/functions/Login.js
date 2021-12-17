7209948213317d9f86f5614e5ece0407a6fe1828
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-17 15:57:22 +0900

    re-styling + fixed album picture problem?

--- src/functions/Login.js
+++ src/functions/Login.js
@@ -5,7 +5,6 @@ import { loginUrl } from "../spotify";
 const Login = () => {
   return (
     <div className="login">
-      <h1>Login Page</h1>
       <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
       <a href={loginUrl}>Login With Spotify</a>
     </div>
