325a014e4f22d9ee690782940fb557131145de74
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-17 15:20:32 +0900

    finished footer, hard coded picture to song playing
    finished rectoring code, some probem happening with Redux i couldn't figure out

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         token: _token,
       });
 
-      spotify.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+      spotify.getPlaylist("37i9dQZEVXcFuHtx5JHln0").then((response) =>
         dispatch({
           type: "SET_DISCOVER_WEEKLY",
           discover_weekly: response,
